9363cbbc8b99a4b263aa46257d01a03e9fe11502
mocos <130593875+MoXcz@users.noreply.github.com>
2025-07-14 19:48:26 -0600

    chore(csv): Clean return

--- file/csv_storage.go
+++ file/csv_storage.go
@@ -124,8 +124,7 @@ func (s *CSVStorage) CompleteTask(w io.Writer, id int) error {
 		return fmt.Errorf("task with ID %d not found", id)
 	}
 
-	err = writeTasksCSV(s.filepath, tasks)
-	return err
+	return writeTasksCSV(s.filepath, tasks)
 }
 
 func (s *CSVStorage) DeleteTask(w io.Writer, id int) error {
