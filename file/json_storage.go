9c6882cc541cda183074d5673b00cc774599423e
mocos <130593875+MoXcz@users.noreply.github.com>
2025-07-14 19:25:47 -0600

    feat(json): List tasks

--- file/json_storage.go
+++ file/json_storage.go
@@ -22,30 +22,13 @@ func NewJSONStorage(filepath string) *JSONStorage {
 }
 
 func (s *JSONStorage) AddTask(task string) error {
-	var tasks []Task
-	file, err := LoadFile(s.filepath)
+	tasks, err := readTasksJSON(s.filepath)
 	if err != nil {
-		return fmt.Errorf("error loading file: %w", err)
+		return err
 	}
-	defer CloseFile(file)
-
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return fmt.Errorf("error getting file info: %w", err)
-	}
-
-	fileContent, err := io.ReadAll(file)
-	if err != nil {
-		return fmt.Errorf("error reading file: %w", err)
-	}
-
-	fmt.Println(string(fileContent))
 
 	lastID := 0
-	if fileInfo.Size() != 0 {
-		if err := json.Unmarshal(fileContent, &tasks); err != nil {
-			return fmt.Errorf("error unmarshalling file: %w", err)
-		}
+	if len(tasks) != 0 {
 		lastID = tasks[len(tasks)-1].ID // should be last task
 	}
 
@@ -66,6 +49,19 @@ func (s *JSONStorage) AddTask(task string) error {
 }
 
 func (s *JSONStorage) ListTasks(w io.Writer) error {
+	tasks, err := readTasksJSON(s.filepath)
+	if err != nil {
+		return err
+	}
+
+	if len(tasks) == 0 {
+		fmt.Fprintln(w, "No tasks found in the file: header only")
+		return nil
+	}
+
+	fmt.Fprintln(w, "Total tasks:", len(tasks))
+	printTasks(w, tasks)
+
 	return nil
 }
 
@@ -75,4 +71,31 @@ func (s *JSONStorage) CompleteTask(w io.Writer, id int) error {
 
 func (s *JSONStorage) DeleteTask(w io.Writer, id int) error {
 	return nil
+}
+
+func readTasksJSON(path string) ([]Task, error) {
+	var tasks []Task
+	file, err := LoadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("error loading file: %w", err)
+	}
+	defer CloseFile(file)
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("error getting file info: %w", err)
+	}
+
+	fileContent, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	if fileInfo.Size() == 0 {
+		if err := json.Unmarshal(fileContent, &tasks); err != nil {
+			return nil, fmt.Errorf("error unmarshalling file: %w", err)
+		}
+		return []Task{}, nil // no tasks, does nil work here?
+	}
+	return tasks, nil
 }
